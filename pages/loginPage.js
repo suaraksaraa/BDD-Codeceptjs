841c061ce574c0b17066187ed3bc1af4bc78143d
Kevan Dharma <kevandharma@gmail.com>
2021-02-01 02:07:38 +0700

    verify text

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -6,13 +6,14 @@ module.exports = {
         submitButton: '//button[@class="btn-custom btn-custom-yellow btn-custom-full submitEmail"]',
         password: '//div[@class="input-wrap password-wrap"]',
         loginButton: '//button[@class="btn-custom btn-custom-yellow btn-custom-full loginSubmitButton"]',
-        verifyAccount: String('Make sure this account is yours')
+        verifyAccount: String('Pastikan kepemilikan akun ini')
     },
     fillFormLogin(){
         I.fillField(this.elements.username, 'tiket-test@mailinator.com');
         I.click(this.elements.submitButton);
         I.fillField(this.elements.password, 'Q1w2e3r4');
         I.click(this.elements.loginButton);
+        pause()
     },
     verifyAccount(){
         I.see(this.elements.verifyAccount);
